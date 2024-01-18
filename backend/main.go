470dfa1a3fda3760f96177a89b4e553367dec3e3
Emerson452 <emerson452pro@gmail.com>
2024-01-18 12:34:25 +0100

    🎨 🏗️ Add new routes

--- backend/main.go
+++ backend/main.go
@@ -7,14 +7,24 @@ import (
 
 func helloWorldPage(w http.ResponseWriter, r *http.Request){
 	fmt.Fprint(w, "Hello World")
+	switch r.URL.Path{
+	case "/":
+		fmt.Fprint(w, "Hello World")
+	case "/ninja": 
+		fmt.F
+	}
 }
 
 func main() {
 // Imprimez dans la console pour indiquer que le serveur a démarré
 fmt.Println("Server is running on :8080")
 
-// Définissez la route "/" pour qu'elle utilise la fonction helloWorldPage
+// Routes
 http.HandleFunc("/", helloWorldPage)
+http.HandleFunc("/Coiffeur", helloWorldPage)
+http.HandleFunc("/Barbier", helloWorldPage)
+http.HandleFunc("/Manucure", helloWorldPage)
+http.HandleFunc("/Institut de beauté", helloWorldPage)
 
 // Démarrer le serveur sur le port 8080
 http.ListenAndServe(":8080", nil)
