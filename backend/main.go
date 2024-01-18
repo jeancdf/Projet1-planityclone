3cd781e1aae9f63ae3b73ec304497738bb6aa03e
Emerson452 <emerson452pro@gmail.com>
2024-01-18 12:22:05 +0100

    🚀 🎨 Improve main.go, deploy a server

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloWorldPage(w http.ResponseWriter, r *http.Request){
+	fmt.Fprint(w, "Hello World")
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+// Imprimez dans la console pour indiquer que le serveur a démarré
+fmt.Println("Server is running on :8080")
+
+// Définissez la route "/" pour qu'elle utilise la fonction helloWorldPage
+http.HandleFunc("/", helloWorldPage)
+
+// Démarrer le serveur sur le port 8080
+http.ListenAndServe(":8080", nil)
 }
