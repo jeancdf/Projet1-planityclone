5c41c76625ec9500d012c34e729e68ff20521b53
jeancdf <61700771+jeancdf@users.noreply.github.com>
2024-01-18 11:30:50 +0100

    Add new routes for salon dashboard, salon profile, and salon slot management

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,15 +3,30 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth.guard';
 import { ClientDashboardComponent } from './client/client-dashboard/client-dashboard.component';
 import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
+import { SalonDashboardComponent } from './salon/salon-dashboard/salon-dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { AdminUserManagmentComponent } from './admin/admin-user-managment/admin-user-managment.component';
 import { AppointmentBookingComponent } from './client/appointment-booking/appointment-booking.component';
 import { SalonListComponent } from './client/salon-list/salon-list.component';
+import { SalonProfileComponent } from './salon/salon-profile/salon-profile.component';
+import { SalonSlotManagementComponent } from './salon/salon-slot-management/salon-slot-management.component';
 
 
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component : SalonListComponent , canActivate: [AuthGuard], data: { role: 'client' }},
+  { path: 'login', component: LoginComponent },
+  { path: 'admin-dashboard', component: AdminDashboardComponent, canActivate: [AuthGuard], data: { role: 'admin' } },
+  { path: 'admin-user-managment', component: AdminUserManagmentComponent, canActivate: [AuthGuard], data: { role: 'admin' } },
+  { path: 'client-dashboard', component: ClientDashboardComponent, canActivate: [AuthGuard], data: { role: 'client' } },
+  { path: 'salon-dashboard', component: SalonDashboardComponent, canActivate: [AuthGuard], data: { role: 'salon' } },
+  { path: 'salon-profile', component: SalonProfileComponent, canActivate: [AuthGuard], data: { role: 'salon' } },
+  { path: 'salon-slot-management', component: SalonSlotManagementComponent, canActivate: [AuthGuard], data: { role: 'salon' } },
+  { path: 'appointment-booking', component: AppointmentBookingComponent, canActivate: [AuthGuard], data: { role: 'client' } },
+  { path: '**', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
