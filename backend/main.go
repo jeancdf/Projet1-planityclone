8308894a9a32fb9488f2b28cc0e130d59f7df44a
Emerson452 <emerson452pro@gmail.com>
2024-01-18 12:37:28 +0100

    🎨 Coiffeur new route

--- backend/main.go
+++ backend/main.go
@@ -9,13 +9,17 @@ func helloWorldPage(w http.ResponseWriter, r *http.Request){
 	fmt.Fprint(w, "Hello World")
 }
 
+func Coiffeur(w http.ResponseWriter, r *http.Request){
+	fmt.Fprint(w, "Coiffeur")
+}
+
 func main() {
 // Imprimez dans la console pour indiquer que le serveur a démarré
 fmt.Println("Server is running on :8080")
 
 // Routes
 http.HandleFunc("/", helloWorldPage)
-http.HandleFunc("/Coiffeur", helloWorldPage)
+http.HandleFunc("/Coiffeur", Coiffeur)
 http.HandleFunc("/Barbier", helloWorldPage)
 http.HandleFunc("/Manucure", helloWorldPage)
 http.HandleFunc("/Institut de beauté", helloWorldPage)
