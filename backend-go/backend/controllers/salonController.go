eb43096a3d54689f77adaa65d22cc723676e8104
Emerson452 <emerson452pro@gmail.com>
2024-01-19 17:15:01 +0100

    ✨ Initialize controller for salon

---
+++ backend-go/backend/controllers/salonController.go
@@ -0,0 +1,17 @@
+package controllers
+
+import (
+	"net/http"
+	"time"
+	"github.com/gin-gonic/gin"
+	"backend/backend/database"
+	"backend/backend/models"
+)
+
+func GeSalons(){}
+
+func CreateSalon(){}
+
+func UpdateSalon(){}
+
+func DeleteSalon(){}
