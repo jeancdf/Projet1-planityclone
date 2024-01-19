f3df7da76e52d67b08a373da9ff750e110075e5b
jeancdf <61700771+jeancdf@users.noreply.github.com>
2024-01-19 14:54:00 +0100

    Add HttpClient and Observable imports, and update DataService methods

--- frontend/src/app/service/data.service.ts
+++ frontend/src/app/service/data.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
+import { AuthService } from './auth.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+  getUsers(): Observable<any> {
+    return this.http.get('/api/users/');
+  }
+
+  getUserById(id: string): Observable<any> {
+    return this.http.get(`/api/users/${id}/`);
+  }
+
+  getSalons(): Observable<any> {
+    return this.http.get('/api/salons/');
+  }
+
+  getSalonById(id: string): Observable<any> {
+    return this.http.get(`/api/salons/${id}/`);
+  }
+
+  
+
+
+
+
 }
