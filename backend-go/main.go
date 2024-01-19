ae0fb673297034d243d84e25c96a64435ffca1f3
dridi14 <dridi.mohamedamine@hxperience.com>
2024-01-19 17:13:20 +0100

    Add migration file for database setup

--- backend-go/main.go
+++ backend-go/main.go
@@ -5,18 +5,15 @@ import (
 	"fmt"
 	"net/http"
 	"github.com/gin-gonic/gin"
-	"backend/backend/database"
 )
 
 func main() {
-	fmt.Println("Server is running on :8000")
-	db := database.Db
-	database.DbMigrate(db)
 	router := gin.New()
 	router.Use(gin.Logger())
-
+	
 	routes.AuthRouter(router)
 	routes.UsersRouter(router)
-
+	
+	fmt.Println("Server is running on :8000")
 	http.ListenAndServe(":8000", router)
 }
