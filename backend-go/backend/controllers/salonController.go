54d777122b305702cbf9bfaccac2e74bfb0ff542
Emerson452 <emerson452pro@gmail.com>
2024-01-19 17:26:52 +0100

    🎨 createsalon update

--- backend-go/backend/controllers/salonController.go
+++ backend-go/backend/controllers/salonController.go
@@ -10,12 +10,24 @@ import (
 
 func GetSalons(c *gin.Context){
 	db := database.Db
-	var salons []models.Salon
+	var salons []userModels.Salon
 	db.Find($salons)
 	c.JSON(http.StatusOk, gin.H{"data": salons})
 }
 
-func CreateSalon(){}
+func CreateSalon(c *gin.Context){
+	db := database.Db
+	var salon userModels.Salon
+
+	c.BindJSON(&salon)
+	if salon.Name == "" || salon.Email == "" || salon.Address == "" || salon.Phone == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Name, Email, Address, and Phone are required"})
+		return
+	}
+
+	db.Create(&salon)
+	c.JSON(http.StatusOK, gin.H{"data": salon})
+}
 
 func UpdateSalon(){}
 
